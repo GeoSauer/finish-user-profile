0922e59f8f232758ffa836f9225bd9770c449e10
GeoSauer <geoffrey.sauer89@gmail.com>
2022-09-27 11:25:33 -0600

    add create profile

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = 'https://rtgjuzfenqqzvbeuakok.supabase.co';
+const SUPABASE_URL = 'https://vskovpzojfjdkywumkvp.supabase.co';
 const SUPABASE_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0Z2p1emZlbnFxenZiZXVha29rIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjA0OTk4NTUsImV4cCI6MTk3NjA3NTg1NX0.8oeztp3xsNKS41oByRVPcWzX-MM8_vXfcKfiEyH8A5s';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZza292cHpvamZqZGt5d3Vta3ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQyOTQ3ODIsImV4cCI6MTk3OTg3MDc4Mn0.euIamkEcpDRax278j2hv-LHL5UpS57qJYvVW1zeQ_GI';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
@@ -31,6 +31,8 @@ export async function signOutUser() {
 
 export async function updateProfile(profile) {
     // > Part A: upsert into profiles table
+    const response = await client.from('profiles').upsert(profile).single();
+    return response;
 }
 
 export async function getProfile(id) {
